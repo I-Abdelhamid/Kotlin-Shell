9cd823f082817b2438e752ba60f8e1e77b178c44
Ibrahim Abdelhamid <ibrahim.abdelhamid@deliveryhero.com>
2025-02-14 17:01:39 +0100

    type cmd

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 fun main() {
+    val builtins = setOf("echo", "exit", "type")
+
     while (true) {
         print("$ ")
         val command = readlnOrNull() ?: break // Read input, exit on EOF
@@ -6,6 +8,14 @@ fun main() {
         when {
             command == "exit 0" -> return // Exit with status 0
             command.startsWith("echo ") -> println(command.removePrefix("echo "))
+            command.startsWith("type ") -> {
+                val target = command.removePrefix("type ")
+                if (builtins.contains(target)) {
+                    println("$target is a shell builtin")
+                } else {
+                    println("$target: not found")
+                }
+            }
             else -> println("$command: command not found")
         }
     }
