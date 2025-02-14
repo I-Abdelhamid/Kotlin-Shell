2f8eea3ffbe99222519d7142b741619300b206ee
Ibrahim Abdelhamid <ibrahim.abdelhamid@deliveryhero.com>
2025-02-14 16:56:20 +0100

    REPL

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main() {
     while (true) {
         print("$ ")
-        val command = readlnOrNull() ?: break  // Read input, exit on EOF
+        val command = readlnOrNull() ?: break // Read input, exit on EOF
         println("$command: command not found")
     }
 }
