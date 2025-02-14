8fee8e6877cd30ce3d4e781324d1f1da266fb0e7
Ibrahim Abdelhamid <ibrahim.abdelhamid@deliveryhero.com>
2025-02-14 16:58:24 +0100

    Exit cmd

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,11 @@ fun main() {
     while (true) {
         print("$ ")
         val command = readlnOrNull() ?: break // Read input, exit on EOF
+
+        if (command == "exit 0") {
+            return // Exit with status 0
+        }
+
         println("$command: command not found")
     }
 }
