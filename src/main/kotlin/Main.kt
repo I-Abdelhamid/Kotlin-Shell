cf204cd906112cfc246fc9650002f4f03c8c9361
Ibrahim Abdelhamid <ibrahim.abdelhamid@deliveryhero.com>
2025-02-15 21:28:15 +0100

    Double quotes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,199 +1,162 @@
 import java.io.File
-import java.nio.file.Path
+import java.nio.file.Files
 import java.nio.file.Paths
-import java.util.LinkedList
-import kotlin.io.path.isExecutable
-import kotlin.io.path.notExists
-import kotlin.system.exitProcess
 
-// Command Interface
-interface Command {
-    fun execute(args: List<String>)
+private var pathCommands: HashMap<String, String> = hashMapOf()
+private var currentDir = Paths.get("").toAbsolutePath()
+
+fun main() {
+    pathCommands = getPathCommands()
+    val commandDispatcher = CommandDispatcher(pathCommands)
+
+    do {
+        val continuePrompt = prompt(commandDispatcher)
+    } while (continuePrompt)
 }
 
-// Command for echo functionality
-class EchoCommand : Command {
-    override fun execute(args: List<String>) {
-        println(args.joinToString(" "))
-    }
+fun String.getPathCommandOrNull(): String? {
+    return pathCommands[this]
 }
 
-// Command for pwd functionality
-class PwdCommand : Command {
-    override fun execute(args: List<String>) {
-        println(System.getProperty("user.dir"))
-    }
+private const val EXIT_COMMAND = "exit 0"
+private const val ECHO_COMMAND = "echo"
+private const val TYPE_COMMAND = "type"
+private const val PWD_COMMAND = "pwd"
+private const val CD_COMMAND = "cd"
+
+private fun prompt(commandDispatcher: CommandDispatcher): Boolean {
+    print("$ ")
+    val command = readln()
+    if (EXIT_COMMAND == command) return false
+
+    val args = parseCommandArguments(command)
+    val firstCommand = args.firstOrNull()
+
+    firstCommand?.let {
+        commandDispatcher.dispatch(it, args)
+    } ?: println("$command: command not found")
+
+    return true
 }
 
-// Command for cd functionality
-class CdCommand(private val directoryContext: DirectoryContext) : Command {
-    override fun execute(args: List<String>) {
-        if (args.isEmpty()) {
-            println("cd: missing argument")
-            return
-        }
-
-        val targetPath = resolvePath(args[0])
-        if (targetPath.notExists()) {
-            println("cd: ${args[0]}: No such file or directory")
-        } else {
-            directoryContext.changeDirectory(targetPath)
-        }
-    }
-
-    private fun resolvePath(path: String): Path {
-        val home = System.getenv("HOME")
-        val dirResolved = path.replace("~", home)
-        return if (dirResolved[0] == '/') {
-            Paths.get(dirResolved)
-        } else {
-            Paths.get(System.getProperty("user.dir"), dirResolved).toAbsolutePath().normalize()
-        }
-    }
-}
-
-// Command for exit functionality
-class ExitCommand : Command {
-    override fun execute(args: List<String>) {
-        exitProcess(0)
-    }
-}
-
-// Command for type functionality
-class TypeCommand(private val path: ShellPath) : Command {
-    private val builtins = setOf("echo", "pwd", "cd", "exit", "type")
-
-    override fun execute(args: List<String>) {
-        if (args.isNotEmpty()) {
-            val cmd = args[0]
-            if (cmd in builtins) {
-                // The command is a shell builtin
-                println("$cmd is a shell builtin")
-            } else {
-                // The command is an external command
-                val executable = findExecutable(path, cmd)
-                if (executable != null) {
-                    println("$cmd is $executable")
-                } else {
-                    println("$cmd: not found")
+fun parseCommandArguments(command: String): List<String> {
+    val sb = StringBuilder()
+    val args = mutableListOf<String>()
+    var current: Char? = null
+    command.toCharArray().forEach { c ->
+        when (current) {
+            '\'' -> if (c == '\'') current = null else sb.append(c)
+            '\"' -> if (c == '\"') current = null else sb.append(c)
+            else -> when {
+                c.isWhitespace() -> {
+                    if (sb.isNotBlank()) {
+                        args.add(sb.toString())
+                        sb.clear()
+                    }
                 }
+                c == '\'' -> current = '\''
+                c == '\"' -> current = '\"'
+                else -> sb.append(c)
+            }
+        }
+    }
+    if (sb.isNotBlank()) args.add(sb.toString())
+    return args
+}
+
+class CommandDispatcher(private val pathCommands: HashMap<String, String>) {
+
+    private val builtInCommands = mapOf(
+        ECHO_COMMAND to ::handleEchoCommand,
+        TYPE_COMMAND to ::handleTypeCommand,
+        PWD_COMMAND to ::handlePwdCommand,
+        CD_COMMAND to ::handleCdCommand
+    )
+
+    fun dispatch(command: String, args: List<String>) {
+        builtInCommands[command]?.invoke(args) ?: run {
+            pathCommands[command]?.let { runExternalCommand(command, args) }
+                ?: println("$command: command not found")
+        }
+    }
+
+    private fun handleEchoCommand(args: List<String>) {
+        println(args.drop(1).joinToString(" ").trim())
+    }
+
+    private fun handleTypeCommand(args: List<String>) {
+        if (args.size >= 2) {
+            val command = args[1]
+            if (command.isBuiltInCommand()) {
+                println("$command is a shell builtin")
+            } else {
+                pathCommands[command]?.let {
+                    println("$command is $it")
+                } ?: println("$command: not found")
             }
         }
     }
 
-    private fun findExecutable(path: ShellPath, cmd: String): Path? {
-        for (dir in path) {
-            try {
-                val p = Paths.get(dir, cmd)
-                if (p.isExecutable()) {
-                    return p
-                }
-            } catch (_: java.nio.file.NoSuchFileException) {}
-        }
-        return null
-    }
-}
-
-
-// Command Registry to register and execute commands
-class CommandRegistry(private val path: ShellPath, private val directoryContext: DirectoryContext) {
-    private val commands = mutableMapOf<String, Command>()
-
-    init {
-        commands["echo"] = EchoCommand()
-        commands["pwd"] = PwdCommand()
-        commands["cd"] = CdCommand(directoryContext)
-        commands["exit"] = ExitCommand()
-        commands["type"] = TypeCommand(path)
+    private fun handlePwdCommand(args: List<String>) {
+        println(currentDir)
     }
 
-    fun executeCommand(command: String, args: List<String>) {
-        val cmd = commands[command]
-        if (cmd != null) {
-            cmd.execute(args)
-        } else {
-            // Handle external commands like 'cat'
-            executeExternalCommand(command, args)
-        }
+    private fun handleCdCommand(args: List<String>) {
+        val dir = args.getOrNull(1)
+        dir?.let { changeDirectory(it) }
     }
 
-    private fun executeExternalCommand(command: String, args: List<String>) {
-        val executable = findExecutable(path, command)
-        if (executable != null) {
-            val executableName = executable.fileName.toString()  // Extract only the command name
-            val process = ProcessBuilder(executableName, *args.toTypedArray())
-                .directory(File(System.getProperty("user.dir")))
-                .inheritIO()
+    private fun runExternalCommand(command: String, args: List<String>) {
+        try {
+            ProcessBuilder(args)
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
                 .start()
-            process.waitFor()
+                .waitFor()
+        } catch (e: Exception) {
+            println("Error running command: $command")
+        }
+    }
+
+    private fun changeDirectory(dir: String) {
+        val newPath = when (dir) {
+            "~" -> Paths.get(System.getenv("HOME"))
+            else -> {
+                val resolvedPath = if (Paths.get(dir).isAbsolute) Paths.get(dir)
+                else currentDir.resolve(dir).normalize()
+                resolvedPath
+            }
+        }
+        if (Files.exists(newPath)) {
+            currentDir = newPath
         } else {
-            println("$command: command not found")
+            println("cd: $newPath: No such file or directory")
         }
     }
 
+    private fun String.isBuiltInCommand(): Boolean {
+        return BuiltInCommands.entries.any { it.name == this.uppercase() }
+    }
+}
 
-    private fun findExecutable(path: ShellPath, cmd: String): Path? {
-        for (dir in path) {
-            try {
-                val p = Paths.get(dir, cmd)
-                if (p.isExecutable()) {
-                    return p
-                }
-            } catch (_: java.nio.file.NoSuchFileException) {}
+enum class BuiltInCommands {
+    EXIT,
+    ECHO,
+    TYPE,
+    PWD,
+    CD
+}
+
+private fun getPathCommands(): HashMap<String, String> {
+    val path = System.getenv()["PATH"]
+    val commands = hashMapOf<String, String>()
+    path?.split(":")
+        ?.map { File(it) }
+        ?.filter { it.exists() && it.isDirectory }
+        ?.forEach { dir ->
+            dir.listFiles()
+                ?.filter { it.isFile && it.canExecute() }
+                ?.forEach { file -> commands.putIfAbsent(file.name, file.absolutePath) }
         }
-        return null
-    }
+    return commands
 }
-
-// Directory Context for managing the current directory state
-class DirectoryContext {
-    var currentDirectory: File = File(System.getProperty("user.dir"))
-
-    fun changeDirectory(newDir: Path) {
-        currentDirectory = newDir.toFile()
-        System.setProperty("user.dir", newDir.toString())
-    }
-}
-
-typealias ShellPath = List<String>
-
-fun main() {
-    val path = System.getenv("PATH").split(":")
-    val directoryContext = DirectoryContext()
-    val commandRegistry = CommandRegistry(path, directoryContext)
-
-    while (true) {
-        prompt()
-        val input = readln() // Wait for user input
-        val inputs = splitShellWords(input)
-        if (inputs.isNotEmpty()) {
-            commandRegistry.executeCommand(inputs[0], inputs.drop(1))
-        }
-    }
-}
-
-fun prompt() {
-    print("$ ")
-}
-
-fun splitShellWords(input: String): List<String> {
-    val matches = WORDS_REGEX.findAll(input)
-    val out = LinkedList<String>()
-    var acc = StringBuilder()
-    for (matchResult in matches) {
-        val (_, word, sq, dq, sep) = matchResult.groups.toList();
-        acc.append((word ?: sq ?: dq)!!.value)
-        if (sep != null) {
-            out.add(acc.toString())
-            acc = StringBuilder()
-        }
-    }
-    return out
-}
-
-const val START_REGEX = """\G\s*"""
-const val UNQUOTED_REGEX = """([^\s\\\'\"]+)"""
-const val SINGLE_QUOTED_REGEX = """'([^\']*)'"""
-const val DOUBLE_QUOTED_REGEX = """"((?:[^\"\\]|\\)*)""""
-const val END_REGEX = """(\s|\z)?"""
-val WORDS_REGEX = "$START_REGEX(?:$UNQUOTED_REGEX|$SINGLE_QUOTED_REGEX|$DOUBLE_QUOTED_REGEX)$END_REGEX".toRegex()
