ee64d1694e4763515e0839ac921b0b0b9c2e9e00
Ibrahim Abdelhamid <ibrahim.abdelhamid@deliveryhero.com>
2025-02-23 17:39:30 +0100

    refactor

--- src/main/kotlin/commands/Command.kt
+++ src/main/kotlin/commands/Command.kt
@@ -1,9 +1,9 @@
 package commands
 
 sealed class Command {
-    object Exit : Command()
+    data object Exit : Command()
     data class Cd(val directory: String?) : Command()
-    object Pwd : Command()
+    data object Pwd : Command()
     data class Type(val argument: String?) : Command()
     data class Echo(val text: String) : Command()
     data class Kill(val pid: String?) : Command()
